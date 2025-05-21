d2f9ade991034376ec873676ddfc9efa846ff15c
jisan5620jk <jisank5620@gmail.com>
2025-05-21 16:04:08 +0600

    Add Pricing Section

--- src/Component6/Pricing/Pricing.jsx
+++ src/Component6/Pricing/Pricing.jsx
@@ -1,6 +1,7 @@
-// Pricing.jsx
 import { useState } from 'react';
 import PricingCard from './PricingCard';
+import shape from '/images/pricing-shape11.png';
+import shape2 from '/images/pricing-shape12.png';
 
 const Pricing = () => {
   const [billing, setBilling] = useState('monthly');
@@ -8,24 +9,27 @@ const Pricing = () => {
   const plans = [
     {
       title: 'SILVER PLAN',
-      price: billing === 'monthly' ? 149 : (149 * 12 * 0.8).toFixed(0),
-      bgColor: 'bg-[#F3F0E9]',
-      textColor: 'text-black',
-      buttonColor: 'bg-[#F3F0E9] text-black hover:opacity-90',
+      price: billing === 'monthly' ? 89 : (89 * 12 * 0.8).toFixed(0),
+      pricingPlan: billing === 'monthly' ? 'month' : 'year',
+      bgColor: 'bg-PrimaryColor4-0 bg-opacity-10',
+      textColor: 'text-HeadingColor-0 group-hover:text-PrimaryColor4-0',
+      buttonColor: 'bg-PrimaryColor4-0 bg-opacity-10 before:bg-PrimaryColor4-0',
     },
     {
       title: 'GOLD PLAN',
       price: billing === 'monthly' ? 159 : (159 * 12 * 0.8).toFixed(0),
-      bgColor: 'bg-[#E1F4EF]',
-      textColor: 'text-green-600',
-      buttonColor: 'bg-green-500 text-white hover:bg-green-600',
+      pricingPlan: billing === 'monthly' ? 'month' : 'year',
+      bgColor: 'bg-PrimaryColor3-0 bg-opacity-10',
+      textColor: 'text-HeadingColor-0 group-hover:text-PrimaryColor3-0',
+      buttonColor: 'bg-PrimaryColor3-0 bg-opacity-10 before:bg-PrimaryColor3-0',
     },
     {
       title: 'DIAMOND PLAN',
-      price: billing === 'monthly' ? 169 : (169 * 12 * 0.8).toFixed(0),
-      bgColor: 'bg-[#EAF4E8]',
-      textColor: 'text-black',
-      buttonColor: 'bg-[#EAF4E8] text-black hover:opacity-90',
+      price: billing === 'monthly' ? 269 : (269 * 12 * 0.8).toFixed(0),
+      pricingPlan: billing === 'monthly' ? 'month' : 'year',
+      bgColor: 'bg-PrimaryColor-0 bg-opacity-10',
+      textColor: 'text-HeadingColor-0 group-hover:text-PrimaryColor-0',
+      buttonColor: 'bg-PrimaryColor-0 bg-opacity-10 before:bg-PrimaryColor-0',
     },
   ];
 
@@ -38,50 +42,74 @@ const Pricing = () => {
   ];
 
   return (
-    <div className='max-w-6xl mx-auto px-4 py-16 text-center'>
-      {/* Toggle Switch */}
-      <div className='flex items-center justify-center space-x-4 mb-10'>
-        <span
-          className={`text-lg font-medium transition-all duration-500 ${
-            billing === 'yearly' ? 'text-gray-900' : 'text-gray-400'
-          }`}
-        >
-          Yearly
-        </span>
-        <button
-          onClick={() =>
-            setBilling(billing === 'monthly' ? 'yearly' : 'monthly')
-          }
-          className={`w-14 h-8 flex items-center rounded-full p-1 transition duration-500 ease-in-out bg-PrimaryColor-0 bg-opacity-10 ${
-            billing === 'monthly' ? '' : ''
-          }`}
-        >
-          <div
-            className={`bg-PrimaryColor-0 w-6 h-6 rounded-full shadow-md transition duration-500 ${
-              billing === 'monthly' ? 'translate-x-6' : ''
+    <section className='py-16 md:py-20 lg:py-28 relative z-10 overflow-hidden'>
+      <img
+        src={shape}
+        draggable={false}
+        alt='Shape'
+        className='absolute -top-12 right-0 animate-wiggle hidden 3xl:block'
+      />
+      <img
+        src={shape2}
+        draggable={false}
+        alt='Shape'
+        className='absolute top-28 left-28 animate-dance hidden 2xl:block'
+      />
+      <div className='Container'>
+        <div className='text-center'>
+          <h5 className='font-Outfit font-medium text-HeadingColor-0 uppercase pl-4 inline-block relative z-10 before:absolute before:top-1/2 before:left-0 before:-translate-y-1/2 before:bg-PrimaryColor-0 before:size-[6px]'>
+            pricing plan
+          </h5>
+          <h1 className='font-Outfit font-semibold text-xl leading-7 sm:text-[34px] sm:leading-[44px] md:text-[44px] md:leading-[54px] lg:text-[30px] lg:leading-[40px] xl:text-[36px] xl:leading-[46px] 2xl:text-[42px] 2xl:leading-[52px] text-HeadingColor-0 mt-2 md:mt-[14px] mb-4'>
+            Comprehensive Course Guide and
+            <br />
+            Best Fee Schedule
+          </h1>
+        </div>
+        {/* Toggle Switch */}
+        <div className='flex items-center justify-center space-x-4 mb-16 mt-10'>
+          <span
+            className={`font-Outfit text-[22px] font-medium transition-all duration-500 ${
+              billing === 'yearly' ? 'text-gray-900' : 'text-gray-400'
             }`}
-          />
-        </button>
-        <span
-          className={`text-lg font-medium transition-all duration-500 ${
-            billing === 'monthly' ? 'text-gray-900' : 'text-gray-400'
-          }`}
-        >
-          Monthly
-        </span>
-      </div>
+          >
+            Yearly
+          </span>
+          <button
+            onClick={() =>
+              setBilling(billing === 'monthly' ? 'yearly' : 'monthly')
+            }
+            className={`w-[90px] h-[46px] flex items-center rounded-full p-1 transition duration-500 ease-in-out bg-PrimaryColor-0 bg-opacity-10 ${
+              billing === 'monthly' ? '' : ''
+            }`}
+          >
+            <div
+              className={`bg-PrimaryColor-0 size-[38px] rounded-full shadow-md transition duration-500 ${
+                billing === 'monthly' ? 'translate-x-11' : ''
+              }`}
+            />
+          </button>
+          <span
+            className={`font-Outfit text-[22px] transition-all duration-500 ${
+              billing === 'monthly' ? 'text-gray-900' : 'text-gray-400'
+            }`}
+          >
+            Monthly
+          </span>
+        </div>
 
-      {/* Pricing Cards */}
-      <div className='grid md:grid-cols-3 gap-8'>
-        {plans.map((plan, index) => (
-          <PricingCard
-            key={index}
-            {...plan}
-            features={features}
-          />
-        ))}
+        {/* Pricing Cards */}
+        <div className='grid md:grid-cols-3 gap-8'>
+          {plans.map((plan, index) => (
+            <PricingCard
+              key={index}
+              {...plan}
+              features={features}
+            />
+          ))}
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
--- src/Component6/Pricing/PricingCard.jsx
+++ src/Component6/Pricing/PricingCard.jsx
@@ -1,38 +1,46 @@
 /* eslint-disable react/prop-types */
 
+import { GiCheckMark } from "react-icons/gi";
+
 const PricingCard = ({
   title,
   price,
+  pricingPlan,
   features,
   bgColor,
   buttonColor,
   textColor,
 }) => {
   return (
-    <div className='rounded-2xl p-6 bg-white flex flex-col items-center shadow-shades'>
-      <div
-        className={`w-full p-6 rounded-xl text-center ${bgColor} ${textColor}`}
-      >
-        <h3 className='text-lg font-semibold mb-2'>{title}</h3>
-        <p className='text-3xl font-bold'>
-          ${price}
-          <span className='text-base font-normal text-gray-600'> / month</span>
+    <div className='group rounded-2xl p-7 bg-white flex flex-col items-center shadow-shades'>
+      <div className={`w-full pt-12 pb-10 rounded-xl text-center ${bgColor}`}>
+        <h3 className='font-Outfit text-xl font-medium mb-2'>{title}</h3>
+        <p className='font-Outfit text-[45px] font-semibold'>
+          <span className={`${textColor} transition-all duration-500`}>
+            ${price}
+          </span>
+          <span className='font-OpenSans text-[17px] text-HeadingColor-0 font-normal'>
+            / {pricingPlan}
+          </span>
         </p>
       </div>
 
-      <ul className='mt-6 space-y-3 text-sm text-gray-800'>
+      <ul className='mt-[50px] mb-[60px] space-y-[22px] text-sm text-gray-800'>
         {features.map((feature, index) => (
           <li
             key={index}
-            className='flex items-center gap-2'
+            className='flex items-center gap-2 font-OpenSans text-[17px] font-medium'
           >
-            <span className='text-green-500 text-lg'>✓</span> {feature}
+            <span className='text-PrimaryColor-0'>
+              <GiCheckMark />
+            </span>
+            {feature}
           </li>
         ))}
       </ul>
 
       <button
-        className={`mt-6 w-full rounded-full py-3 font-semibold transition duration-300 ${buttonColor}`}
+        className={`mb-3 w-full rounded-full py-3 font-Outfit font-medium text-[17px] transition-all duration-500 group-hover:text-white overflow-hidden relative z-10 before:absolute before:right-0 before:top-0 before:w-0 before:h-full before:transition-all before:duration-500 before:-z-10 group-hover:before:w-full group-hover:before:left-0 ${buttonColor}`}
       >
         Choose Plan
       </button>
