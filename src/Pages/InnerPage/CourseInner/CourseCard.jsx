a24c94d8f2570e9e06d71571d3f04c3dca44b2cc
jisan5620jk <jisank5620@gmail.com>
2025-06-02 10:49:33 +0600

    Course Page Design Update

--- src/Pages/InnerPage/CourseInner/CourseCard.jsx
+++ src/Pages/InnerPage/CourseInner/CourseCard.jsx
@@ -1,91 +1,85 @@
 /* eslint-disable react/prop-types */
-import { BsStarFill, BsStarHalf } from 'react-icons/bs';
-import { GoArrowRight } from 'react-icons/go';
-import { IoDocumentTextOutline } from 'react-icons/io5';
-import { PiUsersThree } from 'react-icons/pi';
-import { Link } from 'react-router-dom';
+import { BsStarFill, BsStarHalf } from "react-icons/bs";
+import { GoArrowRight } from "react-icons/go";
+import { IoDocumentTextOutline } from "react-icons/io5";
+import { PiUsersThree } from "react-icons/pi";
+import { Link } from "react-router-dom";
 
 const CourseCard = ({ course, isList }) => {
   return (
     <div
       className={`p-3 lg:p-5 rounded-[20px] bg-white border border-SecondaryColor-0 border-opacity-10 hover:border-PrimaryColor-0 transition-all duration-500 group relative z-10 overflow-hidden !pb-7 ${
-        isList ? 'flex gap-4 !pb-3' : ''
+        isList ? "flex gap-4 !pb-3" : ""
       }`}
     >
       <div
         className={`w-full relative rounded-2xl overflow-hidden ${
-          isList ? 'max-w-[520px]' : ''
+          isList ? "max-w-[520px]" : ""
         }`}
       >
-        <span className='absolute left-[12.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-0'></span>
-        <span className='absolute left-[37.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-[25%]'></span>
-        <span className='absolute left-[62.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-1/2'></span>
-        <span className='absolute left-[87.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-[75%]'></span>
+        <span className="absolute left-[12.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-0"></span>
+        <span className="absolute left-[37.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-[25%]"></span>
+        <span className="absolute left-[62.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-1/2"></span>
+        <span className="absolute left-[87.5%] top-0 h-full w-0 transition-all duration-500 z-10 bg-PrimaryColor-0 group-hover:opacity-0 group-hover:w-[25%] group-hover:left-[75%]"></span>
         <img
           src={course.img}
-          alt='Course'
+          alt="Course"
           draggable={false}
-          className='w-full'
+          className="w-full"
         />
         <span
           className={`absolute z-20 bottom-5 left-5 font-Outfit text-sm px-5 py-[5px] rounded-3xl inline-block text-white bg-PrimaryColor-0 ${
-            isList ? 'hidden' : ''
+            isList ? "hidden" : ""
           }`}
         >
           {course.category}
         </span>
       </div>
 
-      <div className='sm:px-3 pt-6 text-left'>
+      <div className="sm:px-3 pt-6 text-left">
         <span
           className={`font-Outfit text-sm px-5 py-[5px] rounded-3xl font-medium mb-4 hidden text-PrimaryColor-0 bg-PrimaryColor-0 bg-opacity-10 border border-PrimaryColor-0 border-opacity-30 ${
-            isList ? '!inline-block' : ''
+            isList ? "!inline-block" : ""
           }`}
         >
           {course.category}
         </span>
-        <h3 className='font-Outfit text-xl sm:text-2xl lg:text-[22px] xl:text-2xl font-medium text-HeadingColor-0 transition-all duration-500 2xl:mr-12 group-hover:text-PrimaryColor-0'>
+        <h3 className="font-Outfit text-xl sm:text-2xl lg:text-[22px] xl:text-2xl font-medium text-HeadingColor-0 transition-all duration-500 2xl:mr-12 group-hover:text-PrimaryColor-0">
           {course.title}
         </h3>
 
-        <div className='flex items-center gap-1 text-ReviewText-0 mt-3 mb-4'>
+        <div className="flex items-center gap-1 text-ReviewText-0 mt-3 mb-4">
           {[...Array(Math.floor(course.rating))].map((_, i) => (
-            <BsStarFill
-              key={i}
-              className='text-ReviewText-0'
-            />
+            <BsStarFill key={i} className="text-ReviewText-0" />
           ))}
           {course.rating % 1 !== 0 && (
-            <BsStarHalf className='text-ReviewText-0' />
+            <BsStarHalf className="text-ReviewText-0" />
           )}
-          <span className='font-Outfit ml-1 text-HeadingColor-0 text-base'>
+          <span className="font-Outfit ml-1 text-HeadingColor-0 text-base">
             ({course.rating}
             {course.ratingContent})
           </span>
         </div>
-        <div className='text-PrimaryColor-0 text-2xl font-Outfit mb-3'>
+        <div className="text-PrimaryColor-0 text-2xl font-Outfit mb-3">
           {course.price}
         </div>
 
-        <div className='flex items-center gap-3 mb-5 mt-2'>
-          <img
-            src={course.instructorImg}
-            alt='Instructor'
-          />
+        <div className="flex items-center gap-3 mb-5 mt-2">
+          <img src={course.instructorImg} alt="Instructor" />
           <div>
-            <h5 className='font-Outfit text-lg font-medium text-HeadingColor-0'>
+            <h5 className="font-Outfit text-lg font-medium text-HeadingColor-0">
               {course.instructor}
             </h5>
-            <p className='font-OpenSans text-[15px] text-TextColor-0'>
+            <p className="font-OpenSans text-[15px] text-TextColor-0">
               Instructor
             </p>
           </div>
         </div>
 
-        <div className='relative'>
+        <div className="relative">
           <div
             className={`flex items-center gap-10 transition-all duration-500 group-hover:opacity-0 ${
-              isList ? 'group-hover:opacity-100' : ''
+              isList ? "group-hover:opacity-100" : ""
             }`}
           >
             <div
@@ -93,28 +87,25 @@ const CourseCard = ({ course, isList }) => {
             >
               <IoDocumentTextOutline
                 size={18}
-                className='text-PrimaryColor-0'
+                className="text-PrimaryColor-0"
               />
               {course.lessons} Lessons
             </div>
             <div
               className={`font-Outfit text-HeadingColor-0 flex items-center gap-2`}
             >
-              <PiUsersThree
-                size={18}
-                className='text-PrimaryColor-0'
-              />
+              <PiUsersThree size={18} className="text-PrimaryColor-0" />
               {course.students} Students
             </div>
           </div>
-          <Link to='/course'>
+          <Link to="/course">
             <button
-              className={`absolute left-0 -bottom-20 font-Outfit inline-flex items-center gap-2 uppercase text-sm justify-between px-7 py-[12px] bg-PrimaryColor-0 border z-10 overflow-hidden rounded-full text-white transition-all duration-500 group-hover:-bottom-4 ${
-                isList ? '-bottom-16 group-hover:-bottom-16' : ''
+              className={`absolute left-0 font-Outfit inline-flex items-center gap-2 uppercase text-sm justify-between px-7 py-[12px] bg-PrimaryColor-0 border z-10 overflow-hidden rounded-full text-white transition-all duration-500 ${
+                isList ? "-bottom-[70px] hover:bg-PrimaryColor2-0" : "-bottom-20 group-hover:-bottom-4"
               }`}
             >
               Enroll Now
-              <span className='text-xl'>
+              <span className="text-xl">
                 <GoArrowRight />
               </span>
             </button>
--- src/Pages/InnerPage/CourseInner/CourseGrid.jsx
+++ src/Pages/InnerPage/CourseInner/CourseGrid.jsx
@@ -1,223 +1,218 @@
-import { useState } from 'react';
-import CourseCard from './CourseCard';
-import courseImg1 from '/images/case-thumb1.jpg';
-import courseImg2 from '/images/case-thumb2.jpg';
-import courseImg3 from '/images/case-thumb3.jpg';
-import instructorImg from '/images/case-autor.png';
-import instructorImg2 from '/images/case-autor2.png';
-import instructorImg3 from '/images/case-autor3.png';
-import { FaList } from 'react-icons/fa';
-import { LuLayoutGrid } from 'react-icons/lu';
-import { IoSearch } from 'react-icons/io5';
+import { useState } from "react";
+import CourseCard from "./CourseCard";
+import courseImg1 from "/images/case-thumb1.jpg";
+import courseImg2 from "/images/case-thumb2.jpg";
+import courseImg3 from "/images/case-thumb3.jpg";
+import courseImg4 from "/images/course-thumb3.png";
+import courseImg5 from "/images/course-thumb6.png";
+import courseImg6 from "/images/course-thumb5.png";
+import instructorImg from "/images/case-autor.png";
+import instructorImg2 from "/images/case-autor2.png";
+import instructorImg3 from "/images/case-autor3.png";
+import { FaList } from "react-icons/fa";
+import { LuLayoutGrid } from "react-icons/lu";
+import { IoSearch } from "react-icons/io5";
 
 const courses = [
   {
     id: 1,
     img: courseImg1,
-    category: 'Business',
-    title: 'Business Innovation And Development',
-    price: '$30',
+    category: "Business",
+    title: "Business Innovation And Development",
+    price: "$30",
     rating: 4.5,
-    ratingContent: '/3 Ratings',
+    ratingContent: "/3 Ratings",
     lessons: 12,
     students: 1200,
-    instructor: 'John D. Alexon',
+    instructor: "John D. Alexon",
     instructorImg: instructorImg,
-    bgColor: 'bg-PrimaryColor3-0 bg-opacity-[7%]',
-    hoverSpan: 'bg-PrimaryColor3-0',
-    priceBg: 'bg-PrimaryColor3-0',
-    categoryColor: 'text-PrimaryColor3-0',
+    bgColor: "bg-PrimaryColor3-0 bg-opacity-[7%]",
+    hoverSpan: "bg-PrimaryColor3-0",
+    priceBg: "bg-PrimaryColor3-0",
+    categoryColor: "text-PrimaryColor3-0",
     categoryBg:
-      'bg-PrimaryColor3-0 bg-opacity-10 border border-PrimaryColor3-0 border-opacity-30',
-    hoverTitle: 'group-hover:text-PrimaryColor3-0',
-    iconColor: 'text-PrimaryColor3-0',
-    buttonBg: 'bg-PrimaryColor3-0',
+      "bg-PrimaryColor3-0 bg-opacity-10 border border-PrimaryColor3-0 border-opacity-30",
+    hoverTitle: "group-hover:text-PrimaryColor3-0",
+    iconColor: "text-PrimaryColor3-0",
+    buttonBg: "bg-PrimaryColor3-0",
   },
   {
     id: 2,
     img: courseImg2,
-    category: 'Design',
-    title: 'Fundamentals of Network And Domains',
-    price: '$40',
+    category: "Design",
+    title: "Fundamentals of Network And Domains",
+    price: "$40",
     rating: 4.7,
-    ratingContent: '/7 Ratings',
+    ratingContent: "/7 Ratings",
     lessons: 15,
     students: 1500,
-    instructor: 'Jane Smith',
+    instructor: "Jane Smith",
     instructorImg: instructorImg2,
-    bgColor: 'bg-PrimaryColor2-0 bg-opacity-[7%]',
-    hoverSpan: 'bg-PrimaryColor2-0',
-    priceBg: 'bg-PrimaryColor2-0',
-    categoryColor: 'text-PrimaryColor2-0',
+    bgColor: "bg-PrimaryColor2-0 bg-opacity-[7%]",
+    hoverSpan: "bg-PrimaryColor2-0",
+    priceBg: "bg-PrimaryColor2-0",
+    categoryColor: "text-PrimaryColor2-0",
     categoryBg:
-      'bg-PrimaryColor2-0 bg-opacity-10 border border-PrimaryColor2-0 border-opacity-30',
-    hoverTitle: 'group-hover:text-PrimaryColor2-0',
-    iconColor: 'text-PrimaryColor2-0',
-    buttonBg: 'bg-PrimaryColor2-0',
+      "bg-PrimaryColor2-0 bg-opacity-10 border border-PrimaryColor2-0 border-opacity-30",
+    hoverTitle: "group-hover:text-PrimaryColor2-0",
+    iconColor: "text-PrimaryColor2-0",
+    buttonBg: "bg-PrimaryColor2-0",
   },
   {
     id: 3,
     img: courseImg3,
-    category: 'Finance',
-    title: 'Creative Graphic Design with Adobe Suite',
-    price: '$50',
+    category: "Finance",
+    title: "Creative Graphic Design with Adobe Suite",
+    price: "$50",
     rating: 4.8,
-    ratingContent: '/5 Ratings',
+    ratingContent: "/5 Ratings",
     lessons: 10,
     students: 2000,
-    instructor: 'Michael B. Jordan',
+    instructor: "Michael B. Jordan",
     instructorImg: instructorImg3,
-    bgColor: 'bg-PrimaryColor-0 bg-opacity-[7%]',
-    hoverSpan: 'bg-PrimaryColor-0',
-    priceBg: 'bg-PrimaryColor-0',
-    categoryColor: 'text-PrimaryColor-0',
+    bgColor: "bg-PrimaryColor-0 bg-opacity-[7%]",
+    hoverSpan: "bg-PrimaryColor-0",
+    priceBg: "bg-PrimaryColor-0",
+    categoryColor: "text-PrimaryColor-0",
     categoryBg:
-      'bg-PrimaryColor-0 bg-opacity-10 border border-PrimaryColor-0 border-opacity-30',
-    hoverTitle: 'group-hover:text-PrimaryColor-0',
-    iconColor: 'text-PrimaryColor-0',
-    buttonBg: 'bg-PrimaryColor-0',
+      "bg-PrimaryColor-0 bg-opacity-10 border border-PrimaryColor-0 border-opacity-30",
+    hoverTitle: "group-hover:text-PrimaryColor-0",
+    iconColor: "text-PrimaryColor-0",
+    buttonBg: "bg-PrimaryColor-0",
   },
   {
     id: 4,
-    img: courseImg1,
-    category: 'Business',
-    title: 'Business Innovation And Development',
-    price: '$30',
+    img: courseImg4,
+    category: "Business",
+    title: "Business Innovation And Development",
+    price: "$30",
     rating: 4.5,
-    ratingContent: '/3 Ratings',
+    ratingContent: "/3 Ratings",
     lessons: 12,
     students: 1200,
-    instructor: 'John D. Alexon',
+    instructor: "John D. Alexon",
     instructorImg: instructorImg,
-    bgColor: 'bg-PrimaryColor3-0 bg-opacity-[7%]',
-    hoverSpan: 'bg-PrimaryColor3-0',
-    priceBg: 'bg-PrimaryColor3-0',
-    categoryColor: 'text-PrimaryColor3-0',
+    bgColor: "bg-PrimaryColor3-0 bg-opacity-[7%]",
+    hoverSpan: "bg-PrimaryColor3-0",
+    priceBg: "bg-PrimaryColor3-0",
+    categoryColor: "text-PrimaryColor3-0",
     categoryBg:
-      'bg-PrimaryColor3-0 bg-opacity-10 border border-PrimaryColor3-0 border-opacity-30',
-    hoverTitle: 'group-hover:text-PrimaryColor3-0',
-    iconColor: 'text-PrimaryColor3-0',
-    buttonBg: 'bg-PrimaryColor3-0',
+      "bg-PrimaryColor3-0 bg-opacity-10 border border-PrimaryColor3-0 border-opacity-30",
+    hoverTitle: "group-hover:text-PrimaryColor3-0",
+    iconColor: "text-PrimaryColor3-0",
+    buttonBg: "bg-PrimaryColor3-0",
   },
   {
     id: 5,
-    img: courseImg2,
-    category: 'Design',
-    title: 'Fundamentals of Network And Domains',
-    price: '$40',
+    img: courseImg5,
+    category: "Design",
+    title: "Fundamentals of Network And Domains",
+    price: "$40",
     rating: 4.7,
-    ratingContent: '/7 Ratings',
+    ratingContent: "/7 Ratings",
     lessons: 15,
     students: 1500,
-    instructor: 'Jane Smith',
+    instructor: "Jane Smith",
     instructorImg: instructorImg2,
-    bgColor: 'bg-PrimaryColor2-0 bg-opacity-[7%]',
-    hoverSpan: 'bg-PrimaryColor2-0',
-    priceBg: 'bg-PrimaryColor2-0',
-    categoryColor: 'text-PrimaryColor2-0',
+    bgColor: "bg-PrimaryColor2-0 bg-opacity-[7%]",
+    hoverSpan: "bg-PrimaryColor2-0",
+    priceBg: "bg-PrimaryColor2-0",
+    categoryColor: "text-PrimaryColor2-0",
     categoryBg:
-      'bg-PrimaryColor2-0 bg-opacity-10 border border-PrimaryColor2-0 border-opacity-30',
-    hoverTitle: 'group-hover:text-PrimaryColor2-0',
-    iconColor: 'text-PrimaryColor2-0',
-    buttonBg: 'bg-PrimaryColor2-0',
+      "bg-PrimaryColor2-0 bg-opacity-10 border border-PrimaryColor2-0 border-opacity-30",
+    hoverTitle: "group-hover:text-PrimaryColor2-0",
+    iconColor: "text-PrimaryColor2-0",
+    buttonBg: "bg-PrimaryColor2-0",
   },
   {
     id: 6,
-    img: courseImg3,
-    category: 'Finance',
-    title: 'Creative Graphic Design with Adobe Suite',
-    price: '$50',
+    img: courseImg6,
+    category: "Finance",
+    title: "Creative Graphic Design with Adobe Suite",
+    price: "$50",
     rating: 4.8,
-    ratingContent: '/5 Ratings',
+    ratingContent: "/5 Ratings",
     lessons: 10,
     students: 2000,
-    instructor: 'Michael B. Jordan',
+    instructor: "Michael B. Jordan",
     instructorImg: instructorImg3,
-    bgColor: 'bg-PrimaryColor-0 bg-opacity-[7%]',
-    hoverSpan: 'bg-PrimaryColor-0',
-    priceBg: 'bg-PrimaryColor-0',
-    categoryColor: 'text-PrimaryColor-0',
+    bgColor: "bg-PrimaryColor-0 bg-opacity-[7%]",
+    hoverSpan: "bg-PrimaryColor-0",
+    priceBg: "bg-PrimaryColor-0",
+    categoryColor: "text-PrimaryColor-0",
     categoryBg:
-      'bg-PrimaryColor-0 bg-opacity-10 border border-PrimaryColor-0 border-opacity-30',
-    hoverTitle: 'group-hover:text-PrimaryColor-0',
-    iconColor: 'text-PrimaryColor-0',
-    buttonBg: 'bg-PrimaryColor-0',
+      "bg-PrimaryColor-0 bg-opacity-10 border border-PrimaryColor-0 border-opacity-30",
+    hoverTitle: "group-hover:text-PrimaryColor-0",
+    iconColor: "text-PrimaryColor-0",
+    buttonBg: "bg-PrimaryColor-0",
   },
 ];
 const CourseGrid = () => {
   const [isListView, setIsListView] = useState(false);
 
   return (
-    <section className='py-16 md:py-20 lg:py-28'>
-      <div className='Container'>
-        <div className='p-6'>
-          {/* Top Bar */}
-          <div className='flex items-center justify-between flex-wrap gap-7 mb-10'>
-            <div className='flex items-center gap-5 bg-BodyBg-0 rounded-md px-3 py-2 max-w-[420px] w-full'>
-              <div className='flex items-center gap-2'>
-                <button
-                  onClick={() => setIsListView(false)}
-                  className={`size-[38px] flex items-center justify-center text-xl rounded border ${
-                    !isListView
-                      ? 'bg-PrimaryColor-0 text-white'
-                      : 'bg-transparent'
-                  }`}
-                >
-                  <LuLayoutGrid />
-                </button>
-                <button
-                  onClick={() => setIsListView(true)}
-                  className={`size-[38px] flex items-center justify-center text-lg rounded border ${
-                    isListView
-                      ? 'bg-PrimaryColor-0 text-white'
-                      : 'bg-transparent'
-                  }`}
-                >
-                  <FaList />
-                </button>
-              </div>
-              <span className='font-OpenSans text-HeadingColor-0 text-[15px] font-medium'>
-                Showing {courses.length} Result(s)
-              </span>
-            </div>
-
-            <form
-              action='#'
-              className='flex items-center gap-5 bg-BodyBg-0 rounded-md max-w-[320px] w-full relative z-10'
-            >
-              <input
-                type='text'
-                name='search'
-                id='search'
-                placeholder='Search...'
-                className='w-full h-[56px] border border-SecondaryColor-0 border-opacity-10 rounded-md px-5 py-2'
-                required
-              />
+    <section className="py-16 md:py-20 lg:py-28">
+      <div className="Container">
+        {/* Top Bar */}
+        <div className="flex items-center justify-between flex-wrap gap-7 mb-10">
+          <div className="flex items-center gap-5 bg-BodyBg-0 rounded-md px-3 py-2 max-w-[420px] w-full">
+            <div className="flex items-center gap-2">
               <button
-                type='submit'
-                className='absolute top-1/2 -translate-y-1/2 right-7 text-TextColor-0 text-lg'
+                onClick={() => setIsListView(false)}
+                className={`size-[38px] flex items-center justify-center text-xl rounded border ${
+                  !isListView
+                    ? "bg-PrimaryColor-0 text-white"
+                    : "bg-transparent"
+                }`}
               >
-                <IoSearch />
+                <LuLayoutGrid />
               </button>
-            </form>
+              <button
+                onClick={() => setIsListView(true)}
+                className={`size-[38px] flex items-center justify-center text-lg rounded border ${
+                  isListView ? "bg-PrimaryColor-0 text-white" : "bg-transparent"
+                }`}
+              >
+                <FaList />
+              </button>
+            </div>
+            <span className="font-OpenSans text-HeadingColor-0 text-[15px] font-medium">
+              Showing {courses.length} Result(s)
+            </span>
           </div>
 
-          {/* Course Cards */}
-          <div
-            className={
-              isListView
-                ? 'space-y-4'
-                : 'grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'
-            }
+          <form
+            action="#"
+            className="flex items-center gap-5 bg-BodyBg-0 rounded-md max-w-[320px] w-full relative z-10"
           >
-            {courses.map((course) => (
-              <CourseCard
-                key={course.id}
-                course={course}
-                isList={isListView}
-              />
-            ))}
-          </div>
+            <input
+              type="text"
+              name="search"
+              id="search"
+              placeholder="Search..."
+              className="w-full h-[56px] border border-SecondaryColor-0 border-opacity-10 rounded-md px-5 py-2"
+              required
+            />
+            <button
+              type="submit"
+              className="absolute top-1/2 -translate-y-1/2 right-7 text-TextColor-0 text-lg"
+            >
+              <IoSearch />
+            </button>
+          </form>
+        </div>
+
+        {/* Course Cards */}
+        <div
+          className={
+            isListView
+              ? "space-y-4"
+              : "grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6"
+          }
+        >
+          {courses.map((course) => (
+            <CourseCard key={course.id} course={course} isList={isListView} />
+          ))}
         </div>
       </div>
     </section>
